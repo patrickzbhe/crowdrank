74e2d1dc6be595591e9cfa5e56f31172d2d2c65b
Patrick <patrick2hb@hotmail.com>
2021-03-23 11:48:15 -0400

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,11 +5,10 @@ const secret = 'test';
 const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    const isCustomAuth = token.length < 500;
 
     let decodedData;
 
-    if (token && isCustomAuth) {      
+    if (token) {      
       decodedData = jwt.verify(token, secret);
 
       req.userId = decodedData?.id;
